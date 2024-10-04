f1ab21c7d2481bbe43dafc23b0fcd822f2948207
Radovan-geek <131044960+Radovan-geek@users.noreply.github.com>
2024-10-04 21:28:29 +0000

    listo generador de dominios

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big", "happy", "gorgeous"];
+let noun = ["jogger", "racoon", "ornotorrinco", "cat"];
+let extension = [".com", ".net", ".com.ve"];
+
+function generadorDeDominios() {
+  let pronounLoop = Math.floor(Math.random() * pronoun.length);
+  let adjLoop = Math.floor(Math.random() * adj.length);
+  let nounLoop = Math.floor(Math.random() * noun.length);
+  let extensionLoop = Math.floor(Math.random() * extension.length);
+
+  return (
+    pronoun[pronounLoop] +
+    adj[adjLoop] +
+    noun[nounLoop] +
+    extension[extensionLoop]
+  );
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainGen = document.getElementById("domainGenerator");
+  domainGen.innerHTML = generadorDeDominios(pronoun, adj, noun);
 };
