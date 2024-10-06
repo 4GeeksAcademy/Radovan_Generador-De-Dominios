838c2c7d1ac5489e5ecbd258814abb03099b189e
Radovan-geek <131044960+Radovan-geek@users.noreply.github.com>
2024-10-06 19:18:14 +0000

    listo anidado

--- src/app.js
+++ src/app.js
@@ -2,23 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-let pronoun = ["the", "our"];
+let pronoun = ["the", "our", "my", "your"];
 let adj = ["great", "big", "happy", "gorgeous"];
 let noun = ["jogger", "racoon", "ornotorrinco", "cat"];
 let extension = [".com", ".net", ".com.ve"];
 
 function generadorDeDominios() {
   let pronounLoop = Math.floor(Math.random() * pronoun.length);
-  let adjLoop = Math.floor(Math.random() * adj.length);
-  let nounLoop = Math.floor(Math.random() * noun.length);
-  let extensionLoop = Math.floor(Math.random() * extension.length);
-
-  return (
-    pronoun[pronounLoop] +
-    adj[adjLoop] +
-    noun[nounLoop] +
-    extension[extensionLoop]
-  );
+  {
+    let adjLoop = Math.floor(Math.random() * adj.length);
+    {
+      let nounLoop = Math.floor(Math.random() * noun.length);
+      {
+        let extensionLoop = Math.floor(Math.random() * extension.length);
+        {
+          return (
+            pronoun[pronounLoop] +
+            adj[adjLoop] +
+            noun[nounLoop] +
+            extension[extensionLoop]
+          );
+        }
+      }
+    }
+  }
 }
 
 window.onload = function() {
